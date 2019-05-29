4d0277fc502a68c87756d60670b53c880ec21ca0
um-joon-sik <39338850+kimduuukbae@users.noreply.github.com>
2019-05-29 18:21:14 +0900

    Update Engine

--- Library/Define.h
+++ Library/Define.h
@@ -3,7 +3,8 @@
 #include "World.h"
 #include "SceneManager.h"
 #include "Input.h"
-
+#include "SoundManager.h"
 #define D_WORLD		World::instance()
 #define D_SCENE		SceneManager::instance()
 #define D_INPUT		Input::instance()
+#define D_SOUND		SoundManager::instance()
