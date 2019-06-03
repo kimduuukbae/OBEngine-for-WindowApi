9d69fb0a4a82dbd7a3313c1cffa7e751de2860c1
um-joon-sik <39338850+kimduuukbae@users.noreply.github.com>
2019-06-03 16:14:43 +0900

    Update Engine

--- Library/Animation.h
+++ Library/Animation.h
@@ -1,15 +1,27 @@
 #pragma once
+#include "stdafx.h"
 #pragma comment(lib, "msimg32.lib")
 
-class  Animation {
+class __declspec ( dllexport ) Animation {
 public:
+	/*
+		@brief : 사용될 Animation Sprite 들을 지정합니다.
+		@args : count : 사용될 이미지의 갯수, name : 사용될 이미지의 이름들
+	*/
 	void setTexture(int count, const char* name, ...);
 	void setPos(int x, int y);
 	void setblend(float size);
 	void setblendFlag(bool bFlag);
 	void draw(HDC hDC);
 	void update(float dt);
+	/*
+		@breif : Animation 이 몇 초를 주기로 이미지를 변경할 것인지 지정합니다.
+		기본은 1.0f (1초) 입니다.
+	*/
 	void setTime(float time);
+	/*
+		@breif : 이미지의 중심점을 이동합니다. 기본은 (0, 0) 입니다.
+	*/
 	void setOffset(int x, int y);
 	~Animation();
 protected:
@@ -27,5 +39,5 @@ protected:
 
 	float timePlan, time;	// 애니메이션이 바뀌는 시간
 
-	HDC memDC;
+	HDC memDC{ nullptr };
 };
